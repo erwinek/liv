3e4230a619f24e7581a07bf5436a856a6993a970
erwinek <erwinek@o2.pl>
2025-10-10 15:23:15 +0200

    usuniete usage()

--- led-image-viewer.cc
+++ led-image-viewer.cc
@@ -202,50 +202,6 @@ void DisplayAnimation(const FileInfo *file,
   }
 }
 
-static int usage(const char *progname) {
-  fprintf(stderr, "usage: %s [options] <image> [option] [<image> ...]\n",
-          progname);
-
-  fprintf(stderr, "Options:\n"
-          "\t-O<streamfile>            : Output to stream-file instead of matrix (Don't need to be root).\n"
-          "\t-C                        : Center images.\n"
-          "\t-m                        : if this is a stream, mmap() it. This can work around IO latencies in SD-card and refilling kernel buffers. This will use physical memory so only use if you have enough to map file size\n"
-
-          "\nThese options affect images FOLLOWING them on the command line,\n"
-          "so it is possible to have different options for each image\n"
-          "\t-w<seconds>               : Regular image: "
-          "Wait time in seconds before next image is shown (default: 1.5).\n"
-          "\t-t<seconds>               : "
-          "For animations: stop after this time.\n"
-          "\t-l<loop-count>            : "
-          "For animations: number of loops through a full cycle.\n"
-          "\t-D<animation-delay-ms>    : "
-          "For animations: override the delay between frames given in the\n"
-          "\t                            gif/stream animation with this value. Use -1 to use default value.\n"
-          "\t-V<vsync-multiple>        : For animation (expert): Only do frame vsync-swaps on multiples of refresh (default: 1)\n"
-          "\t                            (Tip: use --led-limit-refresh for stable rate)\n"
-
-          "\nOptions affecting display of multiple images:\n"
-          "\t-f                        : "
-          "Forever cycle through the list of files on the command line.\n"
-          "\t-s                        : If multiple images are given: shuffle.\n"
-          );
-
-  fprintf(stderr, "\nGeneral LED matrix options:\n");
-  rgb_matrix::PrintMatrixFlags(stderr);
-
-  fprintf(stderr,
-          "\nSwitch time between files: "
-          "-w for static images; -t/-l for animations\n"
-          "Animated gifs: If both -l and -t are given, "
-          "whatever finishes first determines duration.\n");
-
-  fprintf(stderr, "\nThe -w, -t and -l options apply to the following images "
-          "until a new instance of one of these options is seen.\n"
-          "So you can choose different durations for different images.\n");
-
-  return 1;
-}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
